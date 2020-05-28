1f484251ab95649053cabf232c895ac06a6c3ca0
lindadouw <l.douw@vumc.nl>
2020-05-28 08:51:09 +0000

    Linda review of the code

--- FOOOF_HC.py
+++ FOOOF_HC.py
@@ -2,7 +2,9 @@
 """
 Created on Tue Oct  8 10:25:04 2019
 
-@author: Tianne
+@author: Tianne Numan
+
+# code reviewed by Linda Douw (and Eduarda Centeno as well) on 20200528
 """
 
 # use FOOOF to calculate model with aperiodic part to calculate power spectrum
@@ -65,7 +67,8 @@ std_one_over_f = np.zeros([len(sub_list), 156])
 
 
 sub = 0
-for file in sub_list:
+for file in sub_list: # code review used sub_list[0:2] to include just 2 pts:
+    print(file)
     spectrum = np.loadtxt(dir_files+file)
     for roi in bna_rois:
         # model the power spectrum with FOOOF for each roi
@@ -76,6 +79,8 @@ for file in sub_list:
        AUC_abs_flat_spectrum[sub, roi] = sum(abs(fm._spectrum_flat))
        AUC_abs_orig_spectrum[sub, roi] = sum(abs(fm.power_spectrum))
        AUC_matlab_spectrum[sub, roi] = sum(spectrum[roi, 2:157])
+       # why use 2:157 here? 
+       
        # get offset and slope for each roi and subject
        background_params_offset[sub, roi] = fm.background_params_[0]
        background_params_slope[sub, roi] = fm.background_params_[1]
@@ -84,107 +89,37 @@ for file in sub_list:
     std_flat_spectrum[sub, ] = np.std(flat_spectrum, axis=0)
     mean_std1_flat_spectrum[sub, ] = mean_flat_spectrum[sub, ] + std_flat_spectrum[sub, ]
 
-#    plt.figure()
-#    plt.plot(fm.freqs, mean_flat_spectrum[sub, ], label=sub, color='k')
-#    plt.plot(fm.freqs, mean_std1_flat_spectrum[sub, ], label='std', color='k')
-#    plt.plot(fm.freqs, mean_flat_spectrum[sub, ] - std_flat_spectrum[sub, ], label='std', color='k')
-#    plt.fill_between(fm.freqs, mean_std1_flat_spectrum[sub, ], mean_flat_spectrum[sub, ] - std_flat_spectrum[sub, ], facecolor='dodgerblue', alpha = 0.5)
-#    plt.xlabel('frequency (Hz)')
-#    plt.title('Average FOOOFed spectrum of subject ' + str(sub))
+    # plt.figure()
+    # plt.plot(fm.freqs, mean_flat_spectrum[sub, ], label=sub, color='k')
+    # plt.plot(fm.freqs, mean_std1_flat_spectrum[sub, ], label='std', color='k')
+    # plt.plot(fm.freqs, mean_flat_spectrum[sub, ] - std_flat_spectrum[sub, ], label='std', color='k')
+    # plt.fill_between(fm.freqs, mean_std1_flat_spectrum[sub, ], mean_flat_spectrum[sub, ] - std_flat_spectrum[sub, ], facecolor='dodgerblue', alpha = 0.5)
+    # plt.xlabel('frequency (Hz)')
+    # plt.title('Average FOOOFed spectrum of subject ' + str(sub))
     
     mean_one_over_f[sub, ] = np.mean(one_over_f_spectrum, axis=0)
     std_one_over_f[sub, ] = np.std(one_over_f_spectrum, axis=0)
     
     
-#    plt.figure()
-#    plt.plot(fm.freqs, mean_one_over_f[sub, ], label=sub, color='r')
-#    plt.plot(fm.freqs, mean_one_over_f[sub, ] + std_one_over_f[sub, ], label='std', color='r')
-#    plt.plot(fm.freqs, mean_one_over_f[sub, ] - std_one_over_f[sub, ], label='std', color='r')
-#    plt.fill_between(fm.freqs, mean_one_over_f[sub, ] + std_one_over_f[sub, ], mean_one_over_f[sub, ] - std_one_over_f[sub, ], facecolor='lightcoral', alpha=0.5)
-#    plt.xlabel('frequency (Hz)')
-#    plt.title('1/f plot of subject ' + str(sub))
+    # plt.figure()
+    # plt.plot(fm.freqs, mean_one_over_f[sub, ], label=sub, color='r')
+    # plt.plot(fm.freqs, mean_one_over_f[sub, ] + std_one_over_f[sub, ], label='std', color='r')
+    # plt.plot(fm.freqs, mean_one_over_f[sub, ] - std_one_over_f[sub, ], label='std', color='r')
+    # plt.fill_between(fm.freqs, mean_one_over_f[sub, ] + std_one_over_f[sub, ], mean_one_over_f[sub, ] - std_one_over_f[sub, ], facecolor='lightcoral', alpha=0.5)
+    # plt.xlabel('frequency (Hz)')
+    # plt.title('1/f plot of subject ' + str(sub))
     print(sub) 
     
     mean_orig_spectrum[sub, ] = np.mean(orig_spectrum, axis=0)
     std_orig_spectrum[sub, ] = np.std(orig_spectrum, axis=0)
     
-#    plt.figure()
-#    plt.plot(fm.freqs, mean_orig_spectrum[sub, ], label=sub, color='k')
-#    plt.plot(fm.freqs, mean_orig_spectrum[sub, ] + std_orig_spectrum[sub, ], label='std', color='k')
-#    plt.plot(fm.freqs, mean_orig_spectrum[sub, ] - std_orig_spectrum[sub, ], label='std', color='k')
-#    plt.fill_between(fm.freqs, mean_orig_spectrum[sub, ] + std_orig_spectrum[sub, ], mean_orig_spectrum[sub, ] - std_orig_spectrum[sub, ], facecolor='forestgreen', alpha = 0.5)
-#    plt.xlabel('frequency (Hz)')
-#    plt.title('Original frequency spectrum of subject ' + str(sub))
+    # plt.figure()
+    # plt.plot(fm.freqs, mean_orig_spectrum[sub, ], label=sub, color='k')
+    # plt.plot(fm.freqs, mean_orig_spectrum[sub, ] + std_orig_spectrum[sub, ], label='std', color='k')
+    # plt.plot(fm.freqs, mean_orig_spectrum[sub, ] - std_orig_spectrum[sub, ], label='std', color='k')
+    # plt.fill_between(fm.freqs, mean_orig_spectrum[sub, ] + std_orig_spectrum[sub, ], mean_orig_spectrum[sub, ] - std_orig_spectrum[sub, ], facecolor='forestgreen', alpha = 0.5)
+    # plt.xlabel('frequency (Hz)')
+    # plt.title('Original frequency spectrum of subject ' + str(sub))
     
     sub= sub + 1
-
-# np.corrcoef(AUC_abs_flat_spectrum, AUC_abs_original)
-corr_orig_FOOOFed = np.zeros([len(sub_list), 2])
-
-
-from scipy.stats.stats import pearsonr
-
-sub = 0
-for file in sub_list:
-    corr_orig_FOOOFed[sub, ] = pearsonr(AUC_abs_flat_spectrum[sub, ], AUC_abs_orig_spectrum[sub, ])
-   # corr_orig_FOOOFed = tmp[0]
-   # corr_p_orig_FOOOFed.append = tmp[1]
-    plt.figure()
-    plt.scatter(AUC_abs_flat_spectrum[sub, ], AUC_abs_orig_spectrum[sub, ], label=sub)
-    plt.xlabel('FOOOFed power')
-    plt.ylabel('Original power')
-    plt.title('Correlation FOOOFed and original power of HC')
-    plt.legend()
-    
-    plt.figure()
-    plt.subplot(1,2,1)
-    plt.scatter(AUC_matlab_spectrum[sub, ], AUC_abs_orig_spectrum[sub, ], label=sub)
-    plt.xlabel('Matlab broadband power')
-    plt.ylabel('Original broadband power')
-    plt.subplot(1,2,2)
-    plt.scatter(AUC_matlab_spectrum[sub, ], AUC_abs_flat_spectrum[sub, ], label=sub)
-    plt.xlabel('Matlab broadband power')
-    plt.ylabel('FOOOFed power')
-    
-    sub = sub + 1
-    
-     
- # create scatterplot of background parameters over the 11 subjects and 210 rois
- # repeat the bna rois 11 times, so that you can scatter the same amount of datapoints for x and y
-# x=np.repeat(bna_rois,background_params_offset.shape[0])
-# plt.figure()
-# plt.scatter(x, background_params_offset, )
-
-# BACKGROUND PARAMETERS - OFFSET
- mean_offset = np.mean(background_params_offset,0)
- mean_offset_sub= np.mean()
-plt.figure()
-
-sub =0
-for file in sub_list:
-    plt.figure(1)
-    plt.scatter(bna_rois, background_params_offset[sub, ], label=sub)
-    plt.xlabel('bna rois')
-    plt.ylabel('Offset')
-    plt.title('Background parameter - offset')
-    plt.figure(2)
-    plt.plot(bna_rois, background_params_offset[sub, ], label=sub)
-       
-    sub = sub + 1
-    plt.figure(1)
-plt.scatter(bna_rois, mean_offset, label='mean', s=40, facecolor='k')
-#plt.legend(loc=7)
-
-# BACKGROUND PARAMETERS - SLOPE
-mean_slope = -np.mean(background_params_slope,0)
-plt.figure()
-sub =0
-for file in sub_list:
-    plt.scatter(bna_rois, -background_params_slope[sub, ], label=sub)
-    plt.xlabel('bna rois')
-    plt.ylabel('Slope')
-    plt.title('Background parameter - slope')
-       
-    sub = sub + 1
-plt.scatter(bna_rois,mean_slope, label='mean', s=40, facecolor='k')
 

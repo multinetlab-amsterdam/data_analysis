721aa301d7b8925c606c99df8db89245a33279b8
Tianne <t.numan@amsterdamumc.nl>
2020-09-29 07:26:26 +0000

    added option to run for single ASCII

--- fooof/Compute_broadbandpower_offset_slope.py
+++ fooof/Compute_broadbandpower_offset_slope.py
@@ -43,7 +43,8 @@ from scipy import signal # version 1.4.1
 from fooof import FOOOF # version 0.1.3
 
 
-# ------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
+# Define Functions 
 def get_names_epochs(location, nr_epochs=10, extension='.asc'):
     """ Obtains the names of MEG ASCIIs as saved in folder (e.g., NO-cohorten)
     
@@ -175,16 +176,18 @@ def run_loop_powerspectrum(subject_list, extension='.asc', nr_epochs=10,
     
     Parameters
     ----------
-    subject_list: DataFrame or list
+    subject_list: DataFrame, list, or string
         DataFrame should contain a column 'location_MEG' with the directory of 
         all MEG ASCIIs for each subject. List should contain only 1 column 
-        describing the location. 
+        describing the location. String should be the name of the ASCII
     extension: str, optional
-        Give the extension of ASCIIs, '.txt' or '.asc', default='.asc'
-    nr_rpochs: int, optional
-        Give number of epochs you want to include, default=10
+        Give the extension of ASCIIs, '.txt' or '.asc', default='.asc', 
+        only relevant when giving Dataframe or list
+    nr_epochs: int, optional
+        Give number of epochs you want to include, default=10,
+        not relevant when input is single ascii
     nr_rois: int, optional
-        Give list with  rois you want to analyse, default=np.arange(92)
+        Give list with rois you want to analyse, default=np.arange(92)
     Fs: int, optional
         Sample frequency, default=1250
     window_length: int, optional
@@ -207,32 +210,39 @@ def run_loop_powerspectrum(subject_list, extension='.asc', nr_epochs=10,
     
     mean_pxx = np.empty([len(subject_list), int(window_length/2+1), np.size(nr_rois)])
     broadband_power = np.empty([len(subject_list), np.size(nr_rois)])
-    if ~isinstance(subject_list, pd.DataFrame): 
-        # check if subject_list is dataframe (or list)
-        subject_list=pd.DataFrame(subject_list, columns = ['location_MEG']) 
-        # save dataframe with column 'location'
-    for sub in range(len(subject_list)):
-        asciis = get_names_epochs(subject_list['location_MEG'][sub], nr_epochs, extension)
-        sub_pxx = np.zeros((nr_epochs, int(window_length/2+1), np.size(nr_rois)))
-        mean_pxx[sub,:,:] = 'nan'
-        broadband_power[sub,:] = 'nan'
-        f = np.empty(int(window_length/2+1))
-        print(sub)
-        if len(asciis) == 0:
-            print("no ASCIIs available for: " + subject_list['location_MEG'][sub])
-            continue    
-        for j in range(nr_epochs):
-            timeseries =pd.read_csv(asciis[j][1:-1], index_col=False, 
+    if isinstance(subject_list, str):
+        single_ascii = subject_list
+        timeseries =pd.read_csv(single_ascii, index_col=False, 
                                     header=None, delimiter='\t') 
-            # First and last character should be removed otherwise 
-            # it's not working
-            # Compute power spectrum
-            f, pxx = cal_power_spectrum(timeseries, nr_rois=nr_rois, fs=Fs, 
-                        plot_figure=False, 
-                        title_plot='average power spectrum - epoch: '+ str(j))
-            sub_pxx[j,:,:] = pxx           
-        mean_pxx[sub,:,:] = np.nanmean(sub_pxx, axis=0)    
-        broadband_power[sub,:] = np.sum(mean_pxx[sub,find_nearest(f,
+        f, pxx = cal_power_spectrum(timeseries, nr_rois=nr_rois, fs=Fs, plot_figure=False, title_plot='power spectrum')
+        mean_pxx= pxx            
+        broadband_power = np.sum(mean_pxx[find_nearest(f,freq_range[0]):find_nearest(f, freq_range[1]),:], axis=0)
+    else:
+        if ~isinstance(subject_list, pd.DataFrame): 
+            # check if subject_list is dataframe (or list)
+            subject_list=pd.DataFrame(subject_list, columns = ['location_MEG']) 
+            # save dataframe with column 'location'
+            for sub in range(len(subject_list)):
+                asciis = get_names_epochs(subject_list['location_MEG'][sub], nr_epochs, extension)
+                sub_pxx = np.zeros((nr_epochs, int(window_length/2+1), np.size(nr_rois)))
+                mean_pxx[sub,:,:] = 'nan'
+                broadband_power[sub,:] = 'nan'
+                f = np.empty(int(window_length/2+1))
+                print(sub)
+                if len(asciis) == 0:
+                    print("no ASCIIs available for: " + subject_list['location_MEG'][sub])
+                    continue    
+                for j in range(nr_epochs):
+                    timeseries =pd.read_csv(asciis[j][1:-1], index_col=False, 
+                                    header=None, delimiter='\t') 
+                    # First and last character should be removed otherwise 
+                    # it's not working
+                    # Compute power spectrum
+                    f, pxx = cal_power_spectrum(timeseries, nr_rois=nr_rois, fs=Fs, 
+                                                plot_figure=False, title_plot='average power spectrum - epoch: '+ str(j))
+                    sub_pxx[j,:,:] = pxx           
+                    mean_pxx[sub,:,:] = np.nanmean(sub_pxx, axis=0)    
+                    broadband_power[sub,:] = np.sum(mean_pxx[sub,find_nearest(f,
                                     freq_range[0]):find_nearest(f,
                                     freq_range[1]),:], axis=0)
     return mean_pxx, broadband_power, f  
@@ -286,46 +296,68 @@ os.nice(10)
 # Settings                #   
 ###########################
 
-atlas = 'AAL' # AAL or BNA
-# select either a list of rois (e.g. np.arange(78) when using the cortical 
-# regions of the AAL)
-# or a single roi (e.g. (10,) if you want to evaluate roi 11
-nr_rois = np.arange(78) 
-# give list of rois or 1 rois (AAL cortical = 78, BNA cortical = 210)
-#nr_rois = (10,) # only run for roi 10 # note that python indexes at 0!
+# 1. Select if you want to preprocess 1 single ASCII or multiple
+process_single_ASCII='no' # either 'yes' or 'no'
+# 2a. If you selected yes provide the location and name of the ASCII
+# (e.g. '/data/KNW/NO-cohorten/Scans/sub-9110/meg/T1/AAL/9110_Tr01.asc' )
+single_ascii = '/data/doorgeefluik/mumo_002_OD1_Tr01.asc'
 
-nr_epochs= 10 # number of epochs
-Fs = 1250 # sample frequency
-freq_range=[0.5, 48] # frequency range you want to analyze
-
+# 2b_1. If you selected no; you can create an list of subjects you want to process
+# an example is given here: '/data/KNW/t.numan/GOALS/example_MEG_list.csv'
+# only available for NO-cohorten data!
 subject_list = '/data/KNW/t.numan/GOALS/example_MEG_list.csv'
 dir_input = '/data/KNW/NO-cohorten/Scans/'
+# 2b_2. You can select AAL or BNA atlas
+atlas = 'AAL' # AAL or BNA
+# 2b_3. Select the number of epochs you want to preprocess
+nr_epochs= 10 # number of epochs
+
+# 3. Select which roi or rois you want to analyze
+# if you want to analyze 1 roi, specify its number (nr_rois = (10,))
+nr_rois = (10,) # only run for roi 11 # note that python indexes at 0!
+# if you want to analyze multiple rois, create list with these rois
+# (for example nr_rois = np.arange(78) for all 78 cortical AAL rois)
+
+# 4. Set sample frequency (1250 Hz for Elekta data)
+Fs = 1250 # sample frequency
+# 5. Set frequency range you want to study
+freq_range=[0.5, 48] # frequency range you want to analyze
+
+# 6. Give output directory
 dir_output = '/data/KNW/t.numan/GOALS/tmp/'
+# 7a. Do you want to save the output? 
 save_output = True # you can save output
-name_to_save = 'test' # provide relevant name to save your run
+# 7b. Provide relevant name to save your run
+name_to_save = 'test_mumo_002_roi_11'
 
 
 ###########################
 # Run analysis            # 
 ###########################
 
-# read the csv file containing Case_ID and MM of each included MEG
-# see example '/data/KNW/t.numan/GOALS/example_MEG_list.csv'
-subjects = pd.read_csv(subject_list, delimiter=';', header=0, usecols=[0,1])
-subjects['Case_ID'] = subjects['Case_ID'].apply(lambda x: str(x).zfill(4)) 
-# add padding zeros when necessary
+if process_single_ASCII=='no':
+    # read the csv file containing Case_ID and MM of each included MEG
+    # see example '/data/KNW/t.numan/GOALS/example_MEG_list.csv'
+    subjects = pd.read_csv(subject_list, delimiter=';', header=0, usecols=[0,1])
+    subjects['Case_ID'] = subjects['Case_ID'].apply(lambda x: str(x).zfill(4)) 
+    # add padding zeros when necessary
 
-# get location of ASCIIs of MEG 
-subjects['location_MEG'] = dir_input + 'sub-'+ subjects['Case_ID'] + '/meg/' 
-+ subjects['MM'] + '/' + atlas + '/' 
-
-
-###### Compute power spectrum ######
-## takes a while when multiple subjects are analyzed at once
-mean_pxx, broadband_power, f = run_loop_powerspectrum(subjects, 
+    # get location of ASCIIs of MEG 
+    subjects['location_MEG'] = dir_input + 'sub-'+ subjects['Case_ID'] + '/meg/' + subjects['MM'] + '/' + atlas + '/' 
+    ###### Compute power spectrum ######
+    ## takes a while when multiple subjects are analyzed at once
+    mean_pxx, broadband_power, f = run_loop_powerspectrum(subjects, 
                     nr_epochs=nr_epochs, nr_rois=nr_rois, Fs=Fs, 
                     window_length=4096, freq_range=freq_range)
-       
+    
+elif process_single_ASCII=='yes':
+    mean_pxx, broadband_power, f = run_loop_powerspectrum(single_ascii, 
+                    nr_epochs=1, nr_rois=nr_rois, Fs=Fs, 
+                    window_length=4096, freq_range=freq_range)
+else:
+    print('wrong input process_single_ASCII')
+
+    
 # mean_pxx contains the average power spectra over nr_epochs for all subjects 
 # for all rois, broadband_power is the area under the average power spectra 
 # over the frequency range for all subjects and rois, f gives the frequencies 
@@ -339,35 +371,51 @@ if save_output == True:
     
 
 ###### Compute FOOOOF offset and slope ######
-# create empty arrays to store offset and slope values
-offset = np.empty([mean_pxx.shape[0], mean_pxx.shape[2]]) 
-# index 0 -> number of subjects, index 2 -> number of rois
-slope = np.empty([mean_pxx.shape[0], mean_pxx.shape[2]])
-
-print('Running FOOOF.........................')
-
-# run across all subjects in your list
-for sub in range(len(subjects)):
-    print('subject : ' + str(sub)) # print which subject is analyzed
-    if np.isnan(mean_pxx[sub,1,0]): 
-        # if there is no mean_pxx  for a subject, set offset/slope to nan
-        offset[sub,] = np.nan
-        slope[sub,] = np.nan
-    else:
-        i=0
-        for roi in nr_rois:
-            print('roi : ' + str(roi))
-            offset[sub,i], slope[sub,i] = cal_FOOOF_parameters(mean_pxx[sub,:,i], 
-                                                    f, freq_range=[0.5, 48])
-            i=i+1
-            time.sleep(0.05) 
-            # by having a pause it should limit the %CPU continuously
-    # might be relevant when computing FOOOF for large dataset        
-    if sub % 10 ==0 & save_output==True:
-        print('save offset and slope in between after (another) 10 subjects')
-        np.save(dir_output + name_to_save + '_FOOOF_offset', offset)
-        np.save(dir_output + name_to_save + '_FOOOF_slope', slope)
     
+if process_single_ASCII=='no':    
+    # create empty arrays to store offset and slope values
+    offset = np.empty([mean_pxx.shape[0], mean_pxx.shape[2]]) 
+    # index 0 -> number of subjects, index 2 -> number of rois
+    slope = np.empty([mean_pxx.shape[0], mean_pxx.shape[2]])
+
+    print('Running FOOOF.........................')
+
+    # run across all subjects in your list
+    for sub in range(len(subjects)):
+        print('subject : ' + str(sub)) # print which subject is analyzed
+        if np.isnan(mean_pxx[sub,1,0]): 
+            # if there is no mean_pxx  for a subject, set offset/slope to nan
+            offset[sub,] = np.nan
+            slope[sub,] = np.nan
+        else:
+            i=0
+            for roi in nr_rois:
+                print('roi : ' + str(roi))
+                offset[sub,i], slope[sub,i] = cal_FOOOF_parameters(mean_pxx[sub,:,i], 
+                                                                       f, freq_range=[0.5, 48])
+                i = i + 1
+                time.sleep(0.05) 
+                # by having a pause it should limit the %CPU continuously
+                 # might be relevant when computing FOOOF for large dataset        
+        if sub % 10 ==0 & save_output==True:
+            print('save offset and slope in between after (another) 10 subjects')
+            np.save(dir_output + name_to_save + '_FOOOF_offset', offset)
+            np.save(dir_output + name_to_save + '_FOOOF_slope', slope)
+
+ 
+elif process_single_ASCII=='yes':
+    i=0
+    offset = np.empty(mean_pxx.shape[1])
+    slope = np.empty(mean_pxx.shape[1])
+    for roi in nr_rois:
+        print('roi : ' + str(roi))
+        offset[i], slope[i] = cal_FOOOF_parameters(mean_pxx[:,i], f, freq_range=[0.5, 48])
+        i = i + 1
+        time.sleep(0.05) 
+        # by having a pause it should limit the %CPU continuously
+        # might be relevant when computing FOOOF for large dataset        
+    
+   
 if save_output == True:          
     np.save(dir_output + name_to_save + '_FOOOF_offset', offset)
     np.save(dir_output + name_to_save + '_FOOOF_slope', slope)
@@ -375,14 +423,17 @@ if save_output == True:
     print("FOOOF-offset is saved: "+ dir_output + name_to_save 
           + "_FOOOF_offset")
     print("FOOOF-slope is saved: "+ dir_output + name_to_save + "_FOOOF_slope")
+
     
 ######## Combine results with Case_ID in 'subjects' variable ######
-    
-subjects['mean_broadbandpower'] = np.mean(broadband_power, axis=1)     
-subjects['mean_offset'] = np.mean(offset, axis=1)
-subjects['mean_slope'] = np.mean(slope, axis=1)
+    # only whem multiple cases are processed at once 
+if process_single_ASCII=='no':   
+    subjects['mean_broadbandpower'] = np.mean(broadband_power, axis=1)     
+    subjects['mean_offset'] = np.mean(offset, axis=1)
+    subjects['mean_slope'] = np.mean(slope, axis=1)
 
-subjects.to_csv(dir_output + name_to_save + '_summary.csv' , sep=';', 
+    subjects.to_csv(dir_output + name_to_save + '_summary.csv' , sep=';', 
                 index=False)
-# you can import this .csv file in SPSS or Excel
+    # you can import this .csv file in SPSS or Excel
+    
 
